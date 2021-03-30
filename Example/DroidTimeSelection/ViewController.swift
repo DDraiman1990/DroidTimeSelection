8687b4249841502d7910c51cd7e9965dc3eea60a
DDraiman1990 <ddraiman1990@gmail.com>
2021-03-29 21:15:20 -0500

    issue-14 | Fixed text buttons not being tinted

--- Example/DroidTimeSelection/ViewController.swift
+++ Example/DroidTimeSelection/ViewController.swift
@@ -47,11 +47,14 @@ class ViewController: UIViewController {
     // MARK: - Actions
 
     @IBAction private func onShowTapped(_ sender: Any) {
-        let vc = DroidFactory.Hybrid.viewController(timeFormat: timeFormat, style: .init())
         var style = HybridStyle()
         style.picker.titleColor = .white
-        style.clock.indicatorColor = .blue
-        style.modeButtonTint = .red
+        style.clock.indicatorColor = .white
+        style.modeButtonTint = .white
+        style.cancelButtonContent = .text(title: "CANCEL")
+        style.cancelButtonColor = .white
+        style.submitButtonColor = .white
+        let vc = DroidFactory.Hybrid.viewController(timeFormat: timeFormat, style: style)
         vc.selector.set(time: self.time)
         vc.selector.onCancelTapped = {
             vc.dismiss(animated: true, completion: nil)
